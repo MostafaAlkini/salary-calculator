fe6235259110d98fb5d71059ec6c32806a7f0a36
MostafaAlkini <mostafaalkini@gmail.com>
2025-02-21 19:24:24 +0200

    Declaring the inputs

---
+++ salary-calculator.py
@@ -0,0 +1,7 @@
+# start taking the input from the user
+salary= int(input("Enter your salary: "))
+month= input("Enter the name of the month: ")
+saving=int(input("Enter the percentage of saving: "))
+rent=int(input("Enter the percentage of rent: "))
+electricity=int(input("Enter the percentage of electricity: "))
+
