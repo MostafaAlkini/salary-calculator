dad95de6c80ab4e22d2d15be2b2caa8e9140ffff
MostafaAlkini <mostafaalkini@gmail.com>
2025-02-21 19:36:45 +0200

    Calculating the ammount

--- salary-calculator.py
+++ salary-calculator.py
@@ -5,3 +5,11 @@ saving=int(input("Enter the percentage of saving: "))
 rent=int(input("Enter the percentage of rent: "))
 electricity=int(input("Enter the percentage of electricity: "))
 
+savingAmmount=(salary*saving)/100
+rentAmmount=(salary*rent)/100
+electricityAmmount=(salary*electricity)/100
+
+print("The ammount allocated to saving is: "+str(savingAmmount)+"!")
+print("The ammount allocated to rent is: "+str(rentAmmount)+"!")
+print("The ammount allocated to electricity is: "+str(electricityAmmount)+"!")
+
